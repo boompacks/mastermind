159fb7e8c8d828f3f9a64c5f2646796284df1e7d
gabriele agosta <g.agosta02@gmail.com>
2022-09-22 15:49:57 +0200

    Added basic functionality for user input

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,31 @@
 class Mastermind
   def initialize()
+    @numbers = Array((0..9)) 
+    @code = []
+  end
+
+  private
+  def create_code() 
+    4.times {@code.push(rand(0..9))}
+  end
+
+  public
+  def guess()
+    create_code()
+    p @code
+  end
+
+
+  def create()
   end
 end
 
 game = Mastermind.new()
+choice = ""
+loop do
+  p "Do you want to GUESS or CREATE a code? (Enter guess or create)"
+  choice = gets.chomp.downcase
+  break if choice == "create" || choice == "guess"
+end
+
+choice == "create" ? game.create() : game.guess()
