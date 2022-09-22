a35e36f656a18aea5131b1132e11c5ec136e20b0
gabriele agosta <g.agosta02@gmail.com>
2022-09-22 16:25:14 +0200

    Added basic functionality to play as the codebreaker

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,52 @@
 class Mastermind
   def initialize()
     @numbers = Array((0..9)) 
-    @code = []
+    @computer_code = []
+    @user_code = []
+    @turn = 0
+    @match = false
   end
 
   private
-  def create_code() 
-    4.times {@code.push(rand(0..9))}
+  def random_code() 
+    4.times {@computer_code.push(rand(0..9))}
   end
 
+
+  def input_code()
+    puts "Input the code"
+    4.times do   
+      loop do
+        @user_input = gets.chomp.to_i
+        break if @numbers.include? (@user_input)
+      end
+      @user_code.push(@user_input)
+    end
+  end
+
+
+  def get_user_code(codebreaker)
+    codebreaker == "player" ? input_code() : random_code()
+  end
+
+
+  def win?()
+    @user_code == @computer_code
+  end
+ 
   public
   def guess()
-    create_code()
-    p @code
+    random_code()
+    p @computer_code
+    while @turn < 10 && @match == false
+      @user_code = []
+      get_user_code("player")
+      @match = win?()
+      @turn += 1
+    end
+    @match == true ? @message = "Congratulations, you broke the code" : @message = "The codemaster wins"
+    p @message
   end
-
 
   def create()
   end
@@ -29,3 +61,6 @@ loop do
 end
 
 choice == "create" ? game.create() : game.guess()
+
+# Roba da fare:
+ # Quando l'utente deve indovinare il codice, devo chiedere l'input 
